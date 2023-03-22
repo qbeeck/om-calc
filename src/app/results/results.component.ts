74efd12910c4b8206fade86fa7f695de65128cc4
Artiom Olari <artiom@bend.md>
2023-03-22 16:14:05 +0200

    save

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -6,12 +6,12 @@ import { SettingsService } from '../services/settings.service';
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
-  styleUrls: ['./results.component.scss']
+  styleUrls: ['./results.component.scss'],
 })
 export class ResultsComponent {
-  latency: number[] =  [ 314, 364, 383, 436, 337, 389, 394, 350, 399, 403, 435, 569, 340, 350, 363, 336, 352, 463, 307, 371, 341, 382, 401, 406, 383, 334, 365, 312, 400, 393, 381, 373, 569, 286, 284, 326, 308, 318, 422, 303, 299, 361, 360, 387, 335, 364, 394, 406, 411, 305 ];
-  motor: number[] =  [ 243, 203, 249, 212, 210, 299, 208, 207, 254, 239, 198, 421, 209, 177, 185, 209, 251, 240, 231, 215, 308, 271, 223, 237, 204, 247, 190, 283, 196, 233, 230, 254, 181, 250, 318, 264, 217, 203, 242, 217, 213, 246, 265, 330, 164, 248, 174, 177, 203, 282 ];
-  general: number[] = [ 557, 567, 632, 648, 547, 688, 602, 557, 653, 642, 633, 990, 549, 527, 548, 545, 603, 703, 538, 586, 649, 653, 624, 643, 587, 581, 555, 595, 596, 626, 611, 627, 750, 536, 602, 590, 525, 521, 664, 520, 512, 607, 625, 717, 499, 612, 568, 583, 614, 587 ];
+  latency: number[] = [];
+  motor: number[] = [];
+  general: number[] = [];
 
   latencyAverage = 0;
   motorAverage = 0;
@@ -20,7 +20,7 @@ export class ResultsComponent {
   constructor(
     @Inject(ROUNDS_AMOUNT) public totalRounds: number,
     readonly settings: SettingsService,
-    readonly playground: PlaygroundService,
+    readonly playground: PlaygroundService
   ) {
     const objects = Object.values(this.playground.score);
 
@@ -30,8 +30,11 @@ export class ResultsComponent {
       this.general.push(obj.sevenClicked - obj.signalShowed);
     });
 
-    this.latencyAverage = this.latency.reduce((acc, num) => acc + num, 0) / this.latency.length;
-    this.motorAverage = this.motor.reduce((acc, num) => acc + num, 0) / this.motor.length;
-    this.generalAverage = this.general.reduce((acc, num) => acc + num, 0) / this.general.length;
+    this.latencyAverage =
+      this.latency.reduce((acc, num) => acc + num, 0) / this.latency.length;
+    this.motorAverage =
+      this.motor.reduce((acc, num) => acc + num, 0) / this.motor.length;
+    this.generalAverage =
+      this.general.reduce((acc, num) => acc + num, 0) / this.general.length;
   }
 }
